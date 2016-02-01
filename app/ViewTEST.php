f31e0b0bb26e8b7e6f2bf61bbeb3d6e378db3ea7
winvis <winvissoftware@gmail.com>
2016-02-01 21:48:47 +0900

    バグフィックス

--- app/ViewTEST.php
+++
@@ -1,5 +0,0 @@
-<?php
-  require("view.php");
-  require("../../shapingPrint.php");
-  print_rs(CretatePage("team","Alpha"));
- ?>
--- app/model.php
+++ app/model.php
@@ -1,14 +1,17 @@
 <?php
 
 function ShowTeamMembers($teamname){
+  return RunQuery("SELECT * FROM members WHERE Team=:team",":team",$teamname);
+}
+
+function RunQuery($query,$placeHolder,$param){
   require("../Common.php");
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $stmt = $pdo -> prepare("SELECT * FROM members WHERE Team=:team");
-  $stmt -> bindParam(":team",$teamname);
+  $stmt = $pdo -> prepare($query);
+  $stmt -> bindParam($placeHolder,$param);
   $stmt -> execute();
   $listmembers=$stmt -> fetchALL(PDO::FETCH_ASSOC);
   return $listmembers;
 }
-
 
 ?>
