e10f3151e6e1c486c92abfb9a00b612179109d68
winvis <winvissoftware@gmail.com>
2016-02-02 16:57:09 +0900

    Common.phpの削除

--- Common.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-$pdo = new PDO('mysql:host=127.0.0.1;dbname=planasphere;charset=utf8','root','php1850',
-array(PDO::ATTR_EMULATE_PREPARES => false));
-} catch (PDOException $e) {
- echo('データベース接続失敗。'.$e->getMessage());
- return;
-}
-?>
