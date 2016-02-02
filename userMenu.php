846b833f4ea6129b6d9d37d343eab13497a58548
winvis <winvissoftware@gmail.com>
2016-02-02 15:23:57 +0900

    配列の記述ミスを修正

--- userMenu.php
+++ userMenu.php
@@ -1,18 +1,21 @@
 <?php
 require("ServerConfigManeger.php");
-$login =  $_SESSION["login"];
-$userInfo = ["Guest","icons/guest.png"];
+$login =  $_COOKIE["logined"];
+$userInfo = [["userName"=>"Guest","profileImage"=>"icons/guest.png"]];
 if($login == false){
   $clientAdress= $_SERVER["REMOTE_ADDR"];
   $RegistrationAdress= GetIPAdress($clientAdress);//渡されたIPアドレスをもとにDBへ問い合わせるメソッド
   if(isset($RegistrationAdress)){
-    $userInfo= GetUserName($_cookie["phpsessionid"]); //渡されたセッションIDをもとにDBへ問い合わせるメソッド(戻り値：ユーザー名とアイコンのURL)
-    $_SESSION["login"]==true;
+    $userInfo= GetUserName($_COOKIE["PHPSESSID"]); //渡されたセッションIDをもとにDBへ問い合わせるメソッド(戻り値：ユーザー名とアイコンのURL)
   }
 }
  ?>
  <div  id="user" >
- <img class="user_content" id="user_icon" src=<?php echo $userInfo[1] ?>>
- <span class="user_content" id="user_name"><?php echo $userInfo[0] ?></span>
- <?php require("menu.html"); ?>
+ <img class="user_content" id="user_icon" src=<?php echo $userInfo[0]['profileImage'] ?>>
+ <span class="user_content" id="user_name"><?php echo mb_strimwidth($userInfo[0]['userName'] ,0,10 )?></span>
+ <ul>
+   <li id="uList02" class="login_later">マイページ</li>
+   <li id="uList04" >ログイン</li>
+   <li id="uList05" class="login_later">ログアウト</li>
+ </ul>
  </div>
