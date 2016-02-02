9faef4efb58f53cdb01205d76c99e2c7d4cadb0c
winvis <winvissoftware@gmail.com>
2016-02-02 14:47:05 +0900

    ローカルでのパラメータの修正

--- app/model.php
+++ app/model.php
@@ -1,6 +1,6 @@
 ﻿<?php
 try {
-$pdo = new PDO('mysql:host=127.0.0.1;dbname=winvis;charset=utf8','winvis','hizumin',
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=plnasphere;charset=utf8','root','php1850',
 array(PDO::ATTR_EMULATE_PREPARES => false));
 } catch (PDOException $e) {
  echo('データベース接続失敗。'.$e->getMessage());
