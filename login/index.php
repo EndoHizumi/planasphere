8179c54f5eacb2633a94d842f8b74ac0d616483a
winvis <winvissoftware@gmail.com>
2016-02-02 16:18:10 +0900

    cookieへの書き込み部分の削除

--- login/index.php
+++ login/index.php
@@ -16,17 +16,16 @@
   $TwitterID = $user -> screen_name;
   $sessionID = session_id();
   $ipadress = $_SERVER["REMOTE_ADDR"];
+  $$profileImage = $user -> profile_image_url;
 
   require_once("../app/model.php");
-  $result= RunQueryLite("INSERT INTO users (userID,userName,TwitterID,sessionID,ipadress) VALUES($userID,'$userName','$TwitterID','$sessionID','$ipadress')");
+  $result= RunQueryLite("INSERT INTO users (userID,userName,TwitterID,sessionID,ipadress,profileImage) VALUES($userID,'$userName','$TwitterID','$sessionID','$ipadress','$profileImage')");
   $Chippai = new Chippai();
   $Chippai -> icon = $user -> profile_image_url;
   $Chippai -> Name = $user -> name;
   $Chippai -> mode = $mode;
   $decodefilepath= $Chippai -> Chippai_decode("welcome.php");
   $Chippai -> show($decodefilepath);
-
-  $result = setcookie('logined',true,time()+3600*24*7);
   if($result===true){
     echo("<script> setTimeout('location.replace(\"/plana/main.php\")',10000); </script>");
   }
