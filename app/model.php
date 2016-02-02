6255f101dc8a41f62143cbdf4b035fb5e4446398
winvis <winvissoftware@gmail.com>
2016-02-02 15:06:29 +0900

    ローカルでのパラメータの修正

--- app/model.php
+++ app/model.php
@@ -1,6 +1,6 @@
 ﻿<?php
 try {
-$pdo = new PDO('mysql:host=127.0.0.1;dbname=plnasphere;charset=utf8','root','php1850',
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=planasphere;charset=utf8','root','php1850',
 array(PDO::ATTR_EMULATE_PREPARES => false));
 } catch (PDOException $e) {
  echo('データベース接続失敗。'.$e->getMessage());
@@ -8,7 +8,7 @@ array(PDO::ATTR_EMULATE_PREPARES => false));
 }
 
 function ShowTeamMembers($teamname){
-  return RunQuery("SELECT * FROM plana_members WHERE Team=:team",":team",$teamname);
+  return RunQuery("SELECT * FROM members WHERE Team=:team",":team",$teamname);
 }
 
 function RunQueryLite($query){
