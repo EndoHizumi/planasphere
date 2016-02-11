c43270ff89ac3a638c52a8d8d139e3365c7f3e89
winvis <winvissoftware@gmail.com>
2016-02-12 01:04:46 +0900

    機体登録画面（アップロードページの修正）

--- app/view.php
+++ app/view.php
@@ -3,7 +3,6 @@ function CretatePage($category,$value,$templateFilePath,$func){
   require("../Chippai.php");
   require("model.php");
   $memberLists = $func($value);
-  var_dump($memberLists);
   $Chippai = new Chippai();
   $Chippai -> memberLists = $memberLists;
   $decodefilepath= $Chippai -> Chippai_decode($templateFilePath);
